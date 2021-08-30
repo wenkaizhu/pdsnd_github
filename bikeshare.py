76125d4bc26ef9dc1ca95e76e00026f47a12079a
Wenkai Zhu <wenkai.w.zhu@verizon.com>
2021-08-30 00:03:10 -0400

    Define Month Array

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#define month array
 MONTHS = ['january', 'february', 'march', 'april', 'may', 'june']
 
 DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
